88e005293d0c569cf8529f6c1b47a9fd329939af
Sean Xu <haoranxu@berkeley.edu>
2022-12-28 22:53:18 -0800

    Added Chapter 5 solution, Implemented analytical accelerometer calibration with a bool param for auto_diff or analytical diff Need to change the bool param to config file. TODO Add README

--- workspace/assignments/05-imu-calib/src/imu_tk/src/calibration.cpp
+++ workspace/assignments/05-imu-calib/src/imu_tk/src/calibration.cpp
@@ -85,9 +85,9 @@ template <typename _T1> struct MultiPosAccResidual
       // TODO: implement lower triad model here
       //
       // mis_yz, mis_zy, mis_zx:
-      params[0], params[1], params[2],
-      // mis_xz, mis_xy, mis_yx:
       _T2(0), _T2(0), _T2(0),
+      // mis_xz, mis_xy, mis_yx:
+      params[0], params[1], params[2],
       //    s_x,    s_y,    s_z:
       params[3], params[4], params[5], 
       //    b_x,    b_y,    b_z: 
@@ -110,6 +110,82 @@ template <typename _T1> struct MultiPosAccResidual
   
   const _T1 g_mag_;
   const Eigen::Matrix< _T1, 3 , 1> sample_;
+};
+
+template <typename _T1> 
+class MultiPosAccResidualAnalytic: public ceres::SizedCostFunction<1, 9>
+{
+  public:
+    MultiPosAccResidualAnalytic( 
+    const _T1 &g_mag, 
+    const Eigen::Matrix< _T1, 3 , 1> &sample 
+  ) : g_mag_(g_mag), sample_(sample)
+  {}
+
+  // template <typename _T2>
+  bool Evaluate(double const* const* params,
+                        double* residuals,
+                        double** jacobians) const
+  {
+    // std::cout << "Starting Evaluate" << std::endl;
+    CalibratedTriad_<double> calib_triad( 
+      // mis_yz, mis_zy, mis_zx:
+      double(0), double(0), double(0),
+      // mis_xz, mis_xy, mis_yx:
+      params[0][0], params[0][1], params[0][2],
+      //    s_x,    s_y,    s_z:
+      params[0][3], params[0][4], params[0][5], 
+      //    b_x,    b_y,    b_z: 
+      params[0][6], params[0][7], params[0][8] 
+    );
+    // std::cout << "Starting raw_samp" << std::endl;
+    Eigen::Matrix<double, 3, 1> raw_samp( 
+      double(sample_(0)), 
+      double(sample_(1)), 
+      double(sample_(2)) 
+    );
+
+    // std::cout << "Starting unbiasNormalize" << std::endl;
+    Eigen::Matrix< double, 3 , 1> calib_samp = calib_triad.unbiasNormalize( raw_samp );
+    residuals[0] = double (g_mag_) - calib_samp.norm();
+    
+    // Jacobian Computation Below
+    if (jacobians != nullptr && jacobians[0] != nullptr)
+    {
+      // std::cout << "Starting Jacobian" << std::endl;
+      double Ax = sample_(0), Ay = sample_(1), Az = sample_(2);
+      double Syx = params[0][0],  Szx = params[0][1],  Szy = params[0][2];
+      double kx = params[0][3], ky = params[0][4], kz = params[0][5];
+      double bx = params[0][6], by = params[0][7], bz = params[0][8];
+
+      // std::cout << "Starting Partial" << std::endl;
+      Eigen::Matrix< double, 3 , 1> dadsayx(0, kx*(Ax-bx), 0);
+      Eigen::Matrix< double, 3 , 1> dadsazx(0, 0, -kx*(Ax-bx));
+      Eigen::Matrix< double, 3 , 1> dadsazy(0, 0, ky*(Ay-by));
+      Eigen::Matrix< double, 3 , 1> dadkx(Ax-bx, Syx*(Ax-bx), -Szx*(Ax-bx));
+      Eigen::Matrix< double, 3 , 1> dadky(0, Ay-by, Szy*(Ay-by));
+      Eigen::Matrix< double, 3 , 1> dadkz(0, 0, Az-bz);
+      Eigen::Matrix< double, 3 , 1> dadbx(-kx, -Syx*kx, Szx*kx);
+      Eigen::Matrix< double, 3 , 1> dadby(0, -ky, -Szy*ky);
+      Eigen::Matrix< double, 3 , 1> dadbz(0, 0, -kz);
+
+      // std::cout << "Starting Assignment" << std::endl;
+      jacobians[0][0] = -calib_samp.dot(dadsayx)/calib_samp.norm();
+      jacobians[0][1] = -calib_samp.dot(dadsazx)/calib_samp.norm();
+      jacobians[0][2] = -calib_samp.dot(dadsazy)/calib_samp.norm();
+      jacobians[0][3] = -calib_samp.dot(dadkx)/calib_samp.norm();
+      jacobians[0][4] = -calib_samp.dot(dadky)/calib_samp.norm();
+      jacobians[0][5] = -calib_samp.dot(dadkz)/calib_samp.norm();
+      jacobians[0][6] = -calib_samp.dot(dadbx)/calib_samp.norm();
+      jacobians[0][7] = -calib_samp.dot(dadby)/calib_samp.norm();
+      jacobians[0][8] = -calib_samp.dot(dadbz)/calib_samp.norm();
+    }
+    return true;
+  }
+
+  private:
+  _T1 g_mag_;
+  Eigen::Matrix< _T1, 3 , 1> sample_; 
 };
 
 template <typename _T1> struct MultiPosGyroResidual
@@ -217,10 +293,14 @@ bool MultiPosCalibration_<_T>::calibrateAcc(
     
     //
     // TODO: implement lower triad model here
-    //
-    acc_calib_params[0] = init_acc_calib_.misYZ();
-    acc_calib_params[1] = init_acc_calib_.misZY();
-    acc_calib_params[2] = init_acc_calib_.misZX();
+    // //
+    // acc_calib_params[0] = init_acc_calib_.misYZ();
+    // acc_calib_params[1] = init_acc_calib_.misZY();
+    // acc_calib_params[2] = init_acc_calib_.misZX();
+
+    acc_calib_params[0] = init_acc_calib_.misXZ();
+    acc_calib_params[1] = init_acc_calib_.misXY();
+    acc_calib_params[2] = init_acc_calib_.misYX();
     
     acc_calib_params[3] = init_acc_calib_.scaleX();
     acc_calib_params[4] = init_acc_calib_.scaleY();
@@ -250,12 +330,20 @@ bool MultiPosCalibration_<_T>::calibrateAcc(
     
     if( verbose_output_) cout<<"Trying calibrate... "<<endl;
     
+    bool auto_diff = true;
+
     ceres::Problem problem;
     for( int i = 0; i < static_samples.size(); i++)
     {
-      ceres::CostFunction* cost_function = MultiPosAccResidual<_T>::Create ( 
-        g_mag_, static_samples[i].data() 
-      );
+      ceres::CostFunction* cost_function;
+      if (auto_diff)
+      {
+        cost_function = MultiPosAccResidual<_T>::Create ( 
+          g_mag_, static_samples[i].data() 
+        );
+      }
+      else
+        cost_function = new MultiPosAccResidualAnalytic<_T>(g_mag_, static_samples[i].data());
 
       problem.AddResidualBlock ( 
         cost_function,           /* error fuction */
@@ -291,10 +379,13 @@ bool MultiPosCalibration_<_T>::calibrateAcc(
     //
     // TODO: implement lower triad model here
     // 
+    // min_cost_calib_params[0],
+    // min_cost_calib_params[1],
+    // min_cost_calib_params[2],
+    0,0,0,
     min_cost_calib_params[0],
     min_cost_calib_params[1],
     min_cost_calib_params[2],
-    0,0,0,
     min_cost_calib_params[3],
     min_cost_calib_params[4],
     min_cost_calib_params[5],
